a4d496d42de4f67b3f4a5ebc67cf216b4206f424
Mia Herkt <mia@0x0.st>
2020-11-03 04:01:30 +0100

    Change license to EUPL

--- fhost.py
+++ fhost.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+    Copyright © 2020 Mia Herkt
+    Licensed under the EUPL, Version 1.2 or - as soon as approved
+    by the European Commission - subsequent versions of the EUPL
+    (the "License");
+    You may not use this work except in compliance with the License.
+    You may obtain a copy of the license at:
+
+        https://joinup.ec.europa.eu/software/page/eupl
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied.
+    See the License for the specific language governing permissions
+    and limitations under the License.
+"""
+
 from flask import Flask, abort, escape, make_response, redirect, request, send_from_directory, url_for, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
